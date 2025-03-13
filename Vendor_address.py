22bb2e4d7677ff8a09e3c4198b0c9b442ba2919a
boyu-lby <vincentli.lby@gmail.com>
2025-03-13 14:13:06 -0400

    Added vendor address change automation method

--- Vendor_address.py
+++ Vendor_address.py
@@ -169,7 +169,7 @@ def vendorAddressChangeSingle(driver, account_number: str, address: str, comment
     ).click()
 
     # Switch to the new window
-    for window_handle in driver.
+    for window_handle in driver.window_handles:
         if window_handle != original_window:
             driver.switch_to.window(window_handle)
             break
